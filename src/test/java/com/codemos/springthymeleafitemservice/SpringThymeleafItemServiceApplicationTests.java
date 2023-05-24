854fe737a942dde1230aa1497f6f0d1ef342ab11
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-24 23:13:05 +0900

    SpringThymeleafItemService Application Tests

---
+++ src/test/java/com/codemos/springthymeleafitemservice/SpringThymeleafItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemos.springthymeleafitemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
