5252c15679832b9f3f8a48abc04384634d152d7e
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-28 14:49:18 +0900

    Item

--- src/main/java/com/codemos/springthymeleafitemservice/domain/item/Item.java
+++ src/main/java/com/codemos/springthymeleafitemservice/domain/item/Item.java
@@ -2,12 +2,19 @@ package com.codemos.springthymeleafitemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 	private Long id;
 	private String itemName;
 	private Integer price;
 	private Integer quantity;
+
+	private Boolean open; // Whether to Sale
+	private List<String> regions; // Registration Area
+	private ItemType itemType; // Item Type
+	private String deliveryCode; // Delivery Code
 
 	public Item() {
 	}
